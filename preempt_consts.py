ec1edc5ca462747deb8d2e5aa62802f86a82d97e
Alexandra Lomotan <alomotan@splunk.com>
2019-07-11 14:53:43 -0700

    WIP: initial dev

---
+++ preempt_consts.py
@@ -0,0 +1,12 @@
+# File: preempt_consts.py
+# Copyright (c) 2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+# Define your constants here
+
+ATTRIBUTE_TYPES = {
+    'samAccountName': 'samAccountNames',
+    'primaryDisplayName': 'primaryDisplayNames',
+    'secondaryDisplayName': 'secondaryDisplayNames'
+}
